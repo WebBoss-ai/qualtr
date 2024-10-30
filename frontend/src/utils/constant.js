fba4b9134486e7dc29c410116d4f016700df70f1
WebBoss-ai <111675306+WebBoss-ai@users.noreply.github.com>
2024-10-30 22:05:06 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -8,3 +8,5 @@ export const ANALYTICS_API_END_POINT = "https://qualtr.com/api/v1/analytics";
 export const JOB_ANALYTICS_API_END_POINT = "https://qualtr.com/api/v1/job-analytics";
 export const PORTFOLIO_API_END_POINT = "https://qualtr.com/api/v1/portfolio";
 export const MESSAGE_API_END_POINT = "https://qualtr.com/api/v1/message";
+
+//changed
