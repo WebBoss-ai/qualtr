bd1d21d9310376d3246ca3cbb76b566e16eaeebb
WebBoss-ai <111675306+WebBoss-ai@users.noreply.github.com>
2024-11-16 16:38:39 +0530

    nn

--- backend/routes/jobSeeker.route.js
+++ backend/routes/jobSeeker.route.js
@@ -1,11 +1,10 @@
 // routes/jobSeeker.route.js
 import express from "express";
 import { getAllJobSeekers, getJobSeekerProfile } from "../controllers/user.controller.js";
-import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllJobSeekers); // `/api/v1/job-seekers`
-router.route("/:id").get(isAuthenticated, getJobSeekerProfile); // `/api/v1/job-seekers/:id`
+router.route("/:id").get(getJobSeekerProfile); // `/api/v1/job-seekers/:id`
 
 export default router;
