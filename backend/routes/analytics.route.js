8ebdcc6e7d41206f1869dd1025a22204ad2f11e2
WebBoss-ai <111675306+WebBoss-ai@users.noreply.github.com>
2024-12-14 22:50:04 +0530

    brand dashboard backend correction

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getAnalytics } from "../controllers/analytics.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
-router.route("/").get(getAnalytics);
+router.route("/").get(isAuthenticated,getAnalytics);
 
 export default router;
