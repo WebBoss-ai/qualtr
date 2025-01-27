542177fa232fae8876cce672b0da0a34125cdba2
WebBoss-ai <111675306+WebBoss-ai@users.noreply.github.com>
2025-01-27 19:16:38 +0530

    we

--- backend/utils/passport.js
+++ backend/utils/passport.js
@@ -7,7 +7,7 @@ console.log("Initializing Passport...");
 passport.use(
   new GoogleStrategy(
     {
-        clientID: process.env.GOOGLE_CLIENT_ID || "default-client-id",
+        clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET || "default-client-secret",
         callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:8000/auth/google/callback",
       },
