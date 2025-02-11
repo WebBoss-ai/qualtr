d8d5dec08e4a7f221e15af0f91968d78b7dfb4b4
WebBoss-ai <111675306+WebBoss-ai@users.noreply.github.com>
2025-02-11 22:47:05 +0530

    hgv

--- backend/utils/passport.js
+++ backend/utils/passport.js
@@ -1,6 +1,9 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import DigitalMarketer from "../models/DigitalMarketer.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 console.log("Initializing Passport...");
 
